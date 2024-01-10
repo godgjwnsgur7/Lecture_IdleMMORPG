f2e5c6948959e066848374a1ab91eb5d223a011f
JunHyeok Heo <67333305+godgjwnsgur7@users.noreply.github.com>
2024-01-10 20:18:33 +0900

    Scene

--- Assets/@Scripts/Scenes/BaseScene.cs
+++ Assets/@Scripts/Scenes/BaseScene.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using static Define;
 
-public class BaseScene : MonoBehaviour
+public abstract class BaseScene : InitBase
 {
-    // Start is called before the first frame update
-    void Start()
+    public EScene SceneType { get; protected set; } = EScene.Unknown;
+
+    public override bool Init()
     {
-        
+        if (base.Init() == false)
+            return false;
+
+        Object obj = GameObject.FindObjectOfType(typeof(EventSystem));
+        if (obj == null)
+        {
+            GameObject go = new GameObject() { name = "@EventSystem" };
+            go.AddComponent<EventSystem>();
+            go.AddComponent<StandaloneInputModule>();
+        }
+
+        return true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void Clear();
 }
--- Assets/@Scripts/Scenes/TitleScene.cs
+++ Assets/@Scripts/Scenes/TitleScene.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-public class TitleScene : MonoBehaviour
+public class TitleScene : BaseScene
 {
-    private void Start()
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        SceneType = Define.EScene.TitleScene;
+
+        StartLoadAssets();
+
+        return true;
+    }
+
+    void StartLoadAssets()
     {
         // PreLoad 라벨이 붙은 모든 리소스를 로드하고 콜백함수로 완료 통보를 받음
         Managers.Resource.LoadAllAsync<Object>("PreLoad", (key, count, totalCount) =>
@@ -17,5 +29,10 @@ public class TitleScene : MonoBehaviour
                 // Managers.Data.Init();
             }
         });
+    }
+
+    public override void Clear()
+    {
+
     }
 }
