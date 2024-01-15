d4b8715fe4c0fdb08ce8e92124b8a495fc49d0c3
JunHyeok Heo <67333305+godgjwnsgur7@users.noreply.github.com>
2024-01-15 19:50:56 +0900

    Managers 누락 갱신

--- Assets/@Scripts/Managers/Managers.cs
+++ Assets/@Scripts/Managers/Managers.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 using UnityEngine;
 
 public class Managers : MonoBehaviour
@@ -10,25 +9,28 @@ public class Managers : MonoBehaviour
 
     #region Contents
     private GameManager _game = new GameManager();
+    private ObjectManager _object = new ObjectManager();
 
     public static GameManager Game { get { return Instance?._game; } }
+    public static ObjectManager Object { get { return Instance?._object; } }
     #endregion
 
     #region Core
-    DataManager _data = new DataManager();
-    PoolManager _pool = new PoolManager();
-    ResourceManager _resource = new ResourceManager();
-    SceneManagerEx _scene = new SceneManagerEx();
-    SoundManager _sound = new SoundManager();
-    UIManager _ui = new UIManager();
+    private DataManager _data = new DataManager();
+    private PoolManager _pool = new PoolManager();
+    private ResourceManager _resource = new ResourceManager();
+    private SceneManagerEx _scene = new SceneManagerEx();
+    private SoundManager _sound = new SoundManager();
+    private UIManager _ui = new UIManager();
 
-    public static DataManager Data { get { return Instance._data; } }
-    public static PoolManager Pool { get { return Instance._pool; } }
-    public static ResourceManager Resource { get { return Instance._resource; } }
-    public static SceneManagerEx Scene { get { return Instance._scene; } }
-    public static SoundManager Sound { get { return Instance._sound; } }
-    public static UIManager UI { get { return Instance._ui; } }
+    public static DataManager Data { get { return Instance?._data; } }
+    public static PoolManager Pool { get { return Instance?._pool; } }
+    public static ResourceManager Resource { get { return Instance?._resource; } }
+    public static SceneManagerEx Scene { get { return Instance?._scene; } }
+    public static SoundManager Sound { get { return Instance?._sound; } }
+    public static UIManager UI { get { return Instance?._ui; } }
     #endregion
+
 
     public static void Init()
     {
