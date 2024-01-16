5925f8e672317793eac6a0eced3c507dd5b1a213
JunHyeok Heo <67333305+godgjwnsgur7@users.noreply.github.com>
2024-01-16 18:20:22 +0900

    .

--- Assets/@Scripts/Scenes/GameScene.cs
+++ Assets/@Scripts/Scenes/GameScene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static Define;
 
@@ -16,10 +17,18 @@ public class GameScene : BaseScene
         map.transform.position = Vector3.zero;
         map.name = "@BaseMap";
 
-        Hero hero = Managers.Object.Spawn<Hero>(Vector3.zero);
+        Hero hero = Managers.Object.Spawn<Hero>(new Vector3(-10, -5, 0));
         hero.CreatureState = ECreatureState.Move;
 
+        CameraController camera = Camera.main.GetOrAddComponent<CameraController>();
+        camera.Target = hero;
+
         Managers.UI.ShowBaseUI<UI_Joystick>();
+
+        {
+            Monster monster = Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0));
+            monster.CreatureState = ECreatureState.Idle;
+        }
 
         // TODO
 
